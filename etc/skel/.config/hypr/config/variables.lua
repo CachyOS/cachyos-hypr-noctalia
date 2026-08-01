656f68a318532fa5751e8586a20bd41f5720ddfa
tcz0g <tczog0@t-online.de>
2026-08-01 20:24:07 +0200

    Update variables.lua add flatpak variable

--- etc/skel/.config/hypr/config/variables.lua
+++ etc/skel/.config/hypr/config/variables.lua
@@ -5,6 +5,7 @@ FILE_MANAGER = "dolphin"
 BROWSER      = "firefox"
 EDITOR       = "gnome-text-editor --new-window"
 CALCULATOR   = "gnome-calculator"
+FLATPAK      = "flatpak run "
 
 -- Monitors
 MONITOR1 = ""
