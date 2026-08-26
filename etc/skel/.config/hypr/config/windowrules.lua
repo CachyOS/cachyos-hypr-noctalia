c2b0e3b06b201d91e506436066b9ba0d5a212579
jinra321 <jinra321@gmail.com>
2026-08-25 21:11:29 -0700

    adding Noctalias recommended layer rule

--- etc/skel/.config/hypr/config/windowrules.lua
+++ etc/skel/.config/hypr/config/windowrules.lua
@@ -114,3 +114,15 @@ hl.window_rule({
     },
     no_focus = true,
 })
+
+-- Noctalia layer rule
+hl.layer_rule({
+  name = "noctalia",
+  match = {
+    namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
+  },
+  no_anim = true,
+  ignore_alpha = 0.5,
+  blur = true,
+  blur_popups = true,
+})
